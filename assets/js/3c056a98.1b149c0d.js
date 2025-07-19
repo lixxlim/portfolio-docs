2498388659bb2b99751bb668007f6c5bda0e44f6
lixxlim <lixxlim@users.noreply.github.com>
2025-07-19 01:40:15 +0000

    deploy: 8c432828be9634789107a0aa7aaa3c0d7a534b3a

---
+++ assets/js/3c056a98.1b149c0d.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[4124],{6748:s=>{s.exports=JSON.parse('{"tag":{"label":"SSG","permalink":"/blog/tags/ssg","allTagsPath":"/blog/tags","count":1,"unlisted":false},"listMetadata":{"permalink":"/blog/tags/ssg","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
--- assets/js/9ab478e9.9f9a4d9d.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[8192],{435:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/portfolio-docs/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":2,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
