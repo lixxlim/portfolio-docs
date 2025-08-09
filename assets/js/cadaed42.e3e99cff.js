02663dc1c70064397f185f53d321190e6c4ef2a7
lixxlim <lixxlim@users.noreply.github.com>
2025-08-09 14:35:30 +0000

    deploy: bb04113bd4867ca4324b0ff74cdfed29960558e8

---
+++ assets/js/cadaed42.e3e99cff.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[8735],{934:t=>{t.exports=JSON.parse('{"tag":{"label":"MFT","permalink":"/blog/tags/mft","allTagsPath":"/blog/tags","count":1,"unlisted":false},"listMetadata":{"permalink":"/blog/tags/mft","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
